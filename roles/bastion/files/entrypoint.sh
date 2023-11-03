1b14582f400924965780f661743582db9bc938bc
Ronan-WeScale <ronan.ducamp@wescale.fr>
2023-11-03 17:43:30 +0100

    Update - sudo cmd

--- roles/bastion/files/entrypoint.sh
+++ roles/bastion/files/entrypoint.sh
@@ -3,7 +3,7 @@
 for USRN in $USERS; do
   	echo "Creating user $USRN"
 	useradd -m -s /bin/bash $USRN \
-    && echo "$USRN ALL=(ALL) NOPASSWD: /usr/bin/apt *" >> /etc/sudoers
+    && echo "$USRN ALL=(ALL) NOPASSWD: $SUDO_CMD" >> /etc/sudoers
 done
 
 exec /usr/sbin/sshd -D -e
